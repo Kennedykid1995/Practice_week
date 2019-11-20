ab29d5e6c47943767e32a5f1b7a018bfc93e8a13
Kennedykid1995 <34872885+Kennedykid1995@users.noreply.github.com>
2019-11-19 19:56:55 -0600

    working on basic structure

--- practice_week/src/Weather App/WeekHolder/Day/day.js
+++ practice_week/src/Weather App/WeekHolder/Day/day.js
@@ -1,11 +1,37 @@
 import React from 'react';
-import '../../../App.css'; 
+import '../../../App.css';
 class Day extends React.Component {
 
   render() {
     return (
-      <div className="dayContainer">
-        <h1>Day</h1>
+      <div className="weekHolder">
+        <div className="dayContainer">
+          <h1>Sunday</h1>
+          <section>
+            <h1>Image</h1>
+          </section>
+          <section>
+            <h1>Weather</h1>
+          </section>
+        </div>
+        <div className="dayContainer">
+          <h1>Monday</h1>
+        </div>
+        <div className="dayContainer">
+          <h1>Tuesday</h1>
+        </div>
+        <div className="dayContainer">
+          <h1>Wednesday</h1>
+        </div>
+        <div className="dayContainer">
+          <h1>Thursday</h1>
+        </div>
+        <div className="dayContainer">
+          <h1>Friday</h1>
+        </div>
+        <div className="dayContainer">
+          <h1>Saturday</h1>
+        </div>
       </div>
     );
   }
